492d9815b017f9fa21a3c5b713e48cf926023a05
rafaelsoares626 <rafael.soares626@gmail.com>
2021-04-11 19:01:14 -0300

    Pequeno código teste.
    Código que elabora um contador manual.

---
+++ Contador.java
@@ -0,0 +1,25 @@
+public class Contador
+{
+    int c = 0;    
+    void contar(){
+        c = c + 1;
+    }    
+    void descontar(){
+        if (c > 0){
+            c = c - 1;
+        } else {
+            System.out.println("Não há como descontar mais, pois contador já está zerado!!");
+        }
+    }    
+    void zerar(){
+        if (c != 0){
+            c = 0;
+        } else {
+            System.out.println("O contador já está zerado!!");   
+        }
+    }    
+    int qtd_voltas(){
+        return c;
+    }
+    
+}
